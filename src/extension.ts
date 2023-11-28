a870134ac8308eb6f055c69797a0563adc449171
Bingxi Liang <ben.liang@ericsson.com>
2023-11-28 11:48:19 +0800

    remove code from hello-world demo

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,8 @@
 import { ExtensionContext, commands, window } from "vscode";
-import { HelloWorldPanel } from "./panels/HelloWorldPanel";
 import { DictionaryViewProvider } from "./providers/DictionaryViewProvider";
 
 
 export function activate(context: ExtensionContext) {
-	// register commands
-	let helloPanelDisposable = commands.registerCommand('vscode-dict.helloWorld', () => {
-		HelloWorldPanel.render(context.extensionUri);
-	});
-	context.subscriptions.push(helloPanelDisposable);
-	
 	// register views
 	const dictProvider = new DictionaryViewProvider(context.extensionUri);
 	let dictViewDisposable = window.registerWebviewViewProvider(
